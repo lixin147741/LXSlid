89fa21c2d8c7f0f9b1c04da0ee310113f5128f67
李鑫 <605885892@qq.com>
2015-06-26 18:56:39 +0800

    完成详细页面的显示功能。 待做：1.下拉图片放大。 2.除了首页的页面的数据显示。

---
+++ Global.h
@@ -0,0 +1,13 @@
+//
+//  Global.h
+//  LXSlid
+//
+//  Created by 李鑫 on 15/6/26.
+//  Copyright (c) 2015年 李鑫. All rights reserved.
+//
+
+#ifndef LXSlid_Global_h
+#define LXSlid_Global_h
+
+
+#endif
